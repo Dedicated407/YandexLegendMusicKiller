313d3a7487b85096d33e5f35c480de1d2ebef145
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-10-31 01:24:15 +0300

    feat: added GetAll, Create logic for GenreModel

---
+++ YandexLegendMusicKiller.UI/Pages/Genres/CreateGenreModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YandexLegendMusicKiller.Data.Entities;
+using YandexLegendMusicKiller.Data.Repositories.Genres;
+
+namespace YandexLegendMusicKiller.UI.Pages.Genres;
+
+public class CreateGenreModel : PageModel
+{
+    private readonly IGenresRepository _genresRepository;
+
+    [BindProperty]
+    public Genre Genre { get; set; } = new();
+
+    public CreateGenreModel(IGenresRepository genresRepository)
+    {
+        _genresRepository = genresRepository;
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+    {
+        await _genresRepository.AddAsync(Genre, cancellationToken);
+        return RedirectToPage("Index");
+    }
+}
---
+++ YandexLegendMusicKiller.UI/Pages/Genres/GetAllGenresModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using YandexLegendMusicKiller.Data.Entities;
+using YandexLegendMusicKiller.Data.Repositories.Genres;
+
+namespace YandexLegendMusicKiller.UI.Pages.Genres;
+
+public class GetAllGenresModel : PageModel
+{
+    private readonly IGenresRepository _genreRepository;
+
+    public IEnumerable<Genre>? Genres { get; set; }
+
+    public GetAllGenresModel(IGenresRepository genreRepository)
+    {
+        _genreRepository = genreRepository;
+    }
+
+    public async Task OnGet()
+    {
+        Genres = await _genreRepository.GetAllAsync();
+    }
+}
