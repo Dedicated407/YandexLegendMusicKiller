695ad75f0c5c6dd32e489bf2fdf9e07a09671c33
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-11-03 18:47:40 +0300

    feat: added update for Author

--- YandexLegendMusicKiller.Data/Entities/Common/BaseGuidEntity.cs
+++ YandexLegendMusicKiller.Data/Entities/Common/BaseGuidEntity.cs
@@ -1,9 +1,6 @@
-﻿using System.Text.Json.Serialization;
-
-namespace YandexLegendMusicKiller.Data.Entities.Common;
+﻿namespace YandexLegendMusicKiller.Data.Entities.Common;
 
 public class BaseGuidEntity : BaseEntity
 {
-    [JsonIgnore]
     public Guid Id { get; init; } = Guid.NewGuid();
 }
---
+++ YandexLegendMusicKiller.UI/Pages/Authors/UpdateAuthorModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YandexLegendMusicKiller.Data.Entities;
+using YandexLegendMusicKiller.Data.Repositories.Authors;
+
+namespace YandexLegendMusicKiller.UI.Pages.Authors;
+
+public class UpdateAuthorModel : PageModel
+{
+    private readonly IAuthorsRepository _authorsRepository;
+
+    [BindProperty]
+    public Author? Author { get; set; }
+
+    public UpdateAuthorModel(IAuthorsRepository authorsRepository)
+    {
+        _authorsRepository = authorsRepository;
+    }
+
+    public async Task<IActionResult> OnGetAsync(Guid id, CancellationToken ct)
+    {
+        Author = await _authorsRepository.GetAsync(x => x.Id == id, ct);
+
+        if (Author == null)
+        {
+            return NotFound();
+        }
+
+        return Page();
+    }
+
+    public IActionResult OnPostAsync(CancellationToken cancellationToken)
+    {
+        _authorsRepository.Update(Author!);
+        return RedirectToPage("Index");
+    }
+}
