a262b082cb6bc5853d2051a4c280817eb7309ce9
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-11-04 18:26:16 +0300

    feat: added creating MusicCollection

---
+++ YandexLegendMusicKiller.UI/Pages/MusicCollections/CreateMusicCollectionModel.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YandexLegendMusicKiller.Data.Entities;
+using YandexLegendMusicKiller.Data.Repositories.MusicCollections;
+
+namespace YandexLegendMusicKiller.UI.Pages.MusicCollections;
+
+internal sealed class CreateMusicCollectionModel : PageModel
+{
+    private readonly IMusicCollectionsRepository _musicCollectionsRepository;
+
+    [BindProperty]
+    public MusicCollection MusicCollection { get; set; } = new();
+
+    public CreateMusicCollectionModel(IMusicCollectionsRepository musicCollectionsRepository)
+    {
+        _musicCollectionsRepository = musicCollectionsRepository;
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken ct)
+    {
+        await _musicCollectionsRepository.AddAsync(MusicCollection, ct);
+        return RedirectToPage("Index");
+    }
+}
