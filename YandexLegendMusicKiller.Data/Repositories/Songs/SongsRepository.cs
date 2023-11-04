02688ad66f0e58e4991d5d791a35945404e9f85a
Tsypin Ilya <tsypin.i.p@mail.ru>
2023-11-04 14:58:43 +0300

    feat: added create for Song

--- YandexLegendMusicKiller.Data/Repositories/Songs/SongsRepository.cs
+++ YandexLegendMusicKiller.Data/Repositories/Songs/SongsRepository.cs
@@ -12,13 +12,16 @@ public sealed class SongsRepository : GenericRepository<Song>, ISongsRepository
     }
 
     public async Task<IEnumerable<Song>> GetAllSongsWithAlbumsAndGenresAsync(CancellationToken ct = default)
-        => await EntitySet.Include(x => x.Album)
+        => await EntitySet
+            .Include(x => x.Album)
+            .Include(x => x.Album.Author)
             .Include(x => x.Genre)
             .ToListAsync(ct);
 
     public async Task<IEnumerable<Song>> GetAllSongsWithAlbumsAndGenresAsync(Expression<Func<Song, bool>> expression, 
         CancellationToken ct = default)
         => await EntitySet.Include(x => x.Album)
+            .Include(x => x.Album.Author)
             .Include(x => x.Genre)
             .Where(expression)
             .ToListAsync(ct);
---
+++ YandexLegendMusicKiller.UI/Pages/Songs/CreateSongModel.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YandexLegendMusicKiller.Data.Entities;
+using YandexLegendMusicKiller.Data.Repositories.Albums;
+using YandexLegendMusicKiller.Data.Repositories.Genres;
+using YandexLegendMusicKiller.Data.Repositories.Songs;
+
+namespace YandexLegendMusicKiller.UI.Pages.Songs;
+
+public class CreateSongModel : PageModel
+{
+    private readonly ISongsRepository _songsRepository;
+    private readonly IAlbumsRepository _albumsRepository;
+    private readonly IGenresRepository _genresRepository;
+
+    [BindProperty]
+    public Song Song { get; set; } = new();
+
+    [BindProperty]
+    public Guid AlbumId { get; set; }
+
+    [BindProperty]
+    public string GenreId { get; set; } = null!;
+
+    public List<SelectListItem>? Albums { get; private set; }
+
+    public List<SelectListItem>? Genres { get; private set; }
+
+    public CreateSongModel(ISongsRepository songsRepository, IAlbumsRepository albumsRepository, 
+        IGenresRepository genresRepository)
+    {
+        _songsRepository = songsRepository;
+        _albumsRepository = albumsRepository;
+        _genresRepository = genresRepository;
+    }
+
+    public async Task OnGet(CancellationToken ct)
+    {
+        var albumsData = await _albumsRepository.GetAllAlbumsWithAuthorsAsync(ct);
+        Albums = albumsData.Select(x => new SelectListItem 
+        {
+            Value = x.Id.ToString(),
+            Text = $"Альбом: {x.Name} Исполнитель: {x.Author.NickName}"
+        }).ToList();
+
+        var genresData = await _genresRepository.GetAllAsync(ct);
+        Genres = genresData.Select(x => new SelectListItem
+        {
+            Value = x.Name,
+            Text = x.Name
+        }).ToList();
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken ct)
+    {
+        Song.AlbumId = AlbumId;
+        Song.GenreId = GenreId;
+        await _songsRepository.AddAsync(Song, ct);
+        return RedirectToPage("Index");
+    }
+}
